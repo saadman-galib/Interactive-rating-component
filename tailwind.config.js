e0360de8839905831cc47a21ae7e157167baa1c4
saadman-galib <galib2829@gmail.com>
2022-07-14 14:01:02 +0600

    Feedback done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'white': "hsl(0, 0%, 100%)",
+                "light-grey": "hsl(217, 12%, 63%)",
+                "medium-grey": "hsl(216, 12%, 54%)",
+                "dark-blue": "hsl(213, 19%, 18%)",
+                "very-dark-blue": "hsl(216, 12%, 8%)",
+                "card-blue": "rgb(30,38,48)",
+            },
+            fontFamily: {
+                overpass: ["Overpass", "sans-serif"],
+            },
+            plugins: [],
+        },
+    },
+};
